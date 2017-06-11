88e7a92d48d3a09ff1b3ab7d2211e43649bc2bc9
BasicCoder <abasiccoder@gmail.com>
2017-06-11 11:05:04 +0800

    update download data shell

--- data/DownloadData.sh
+++ data/DownloadData.sh
@@ -1,2 +1,5 @@
-$ curl https://pjreddie.com/media/files/mnist_train.csv -o data/mnist_train.csv
-$ curl https://pjreddie.com/media/files/mnist_test.csv -o data/mnist_test.csv
+#!/bin/bash
+#download train data
+curl https://pjreddie.com/media/files/mnist_train.csv -o data/mnist_train.csv
+#downloadd test data
+curl https://pjreddie.com/media/files/mnist_test.csv -o data/mnist_test.csv
