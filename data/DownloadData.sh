8242f728822ff4e21026bf84d67c1394a2dac48d
BasicCoder <abasiccoder@gmail.com>
2017-06-11 10:37:20 +0800

    initial repository

---
+++ data/DownloadData.sh
@@ -0,0 +1,2 @@
+$ curl https://pjreddie.com/media/files/mnist_train.csv -o data/mnist_train.csv
+$ curl https://pjreddie.com/media/files/mnist_test.csv -o data/mnist_test.csv
---
+++ evaluate.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import tensorflow as tf 
+
+import Mnist_CSV_CNN.mnist as mnist
+from Mnist_CSV_CNN.model import Model
+
+FLAGS = tf.app.flags.FLAGS 
+
+def evaluate():
+    with tf.Graph().as_default():
+        images, labels = mnist.load_test_data(FLAGS.test_data)
+        model = Model()
+
+        logits = model.inference(images, keep_prob = 1.0)
+        accuracy = model.accuracy(logits, labels)
+
+        saver = tf.train.Saver()
+        with tf.Session() as sess:
+            tf.global_variables_initializer().run()
+            saver.restore(sess, FLAGS.checkpoint_file_path)
+
+            total_accuracy = sess.run([accuracy])
+            print('Test accuracy: {}'.format(total_accuracy))
+
+def main(argv = None):
+    evaluate()
+
+if __name__ == '__main__':
+    tf.app.flags.DEFINE_string('checkpoint_file_path', 'checkpoints/model.ckpt-10000', 'path to checkpoint file')
+    tf.app.flags.DEFINE_string('test_data', 'data/mnist_test.csv', 'path to test data')
+
+    tf.app.run()
